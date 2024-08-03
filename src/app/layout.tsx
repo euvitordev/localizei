651363489074bb186f328ef82f566e251df7c6b2
euvitordev <dev.vitorlucas@gmail.com>
2024-08-03 20:05:07 -0300

    feat: name adjustment and addition of (PopupWelcome and Toaster)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
+import { PopupWelcome } from "@/components/PopupWelcome"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Toaster } from "sonner"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Fui de CEP",
+  title: "Localizei",
   description:
-    "Fui de CEP, o seu guia para localizar destinos, sem complicação!",
+    "Localizei, o seu guia para localizar destinos, sem complicação!",
 }
 
 export default function RootLayout({
@@ -16,8 +18,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br" className="dark" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Toaster richColors position="top-right" />
+        <PopupWelcome />
+        {children}
+      </body>
     </html>
   )
 }
